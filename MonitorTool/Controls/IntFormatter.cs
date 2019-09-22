1f06ca95fa037761ee3bb815150ebf517a01712d
YangDR <ydrml@hotmail.com>
2019-09-22 17:17:34 +0800

    支持修改保存数量

---
+++ MonitorTool/Controls/IntFormatter.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using Windows.UI.Xaml.Data;
+
+namespace MonitorTool.Controls {
+    internal class IntFormatter : IValueConverter {
+        public object Convert(object value, Type targetType, object parameter, string language)
+            => value.ToString();
+
+        public object ConvertBack(object value, Type targetType, object parameter, string language) {
+            var text = ((string) value).ToLower();
+            return int.TryParse(text, out var result)
+                ? result
+                : text == "inf"
+                  || text == "+inf"
+                  || text == "∞"
+                  || text == "+∞"
+                    ? int.MaxValue
+                    : text == "-inf"
+                      || text == "-∞"
+                        ? int.MinValue
+                        : 0;
+        }
+    }
+}
